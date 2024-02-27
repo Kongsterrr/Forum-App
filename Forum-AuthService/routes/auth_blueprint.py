4a96b133cef6b01077f500b0c61bb1c68426149f
Jack <jackkong@bu.edu>
2024-02-27 00:05:28 -0500

    modify the login:get_user_token_and_type to get token from AuthService

--- Forum-AuthService/routes/auth_blueprint.py
+++ Forum-AuthService/routes/auth_blueprint.py
@@ -1,7 +1,7 @@
 from flask import Blueprint
 from controllers.auth_controller import CodeVerifyView, SendEmailView, TokenVerifyView, UserRegisterView
 
-auth_blueprint = Blueprint('authentication', __name__)
+auth_blueprint = Blueprint('authentication', __name__, url_prefix='/authentication')
 auth_blueprint.add_url_rule('/register', view_func=UserRegisterView.as_view('auth-register'), methods=['POST'])
 auth_blueprint.add_url_rule('/email/send', view_func=SendEmailView.as_view('auth-email-send'), methods=['POST'])
 auth_blueprint.add_url_rule('/email/verify', view_func=CodeVerifyView.as_view('auth-email'), methods=['POST'])
--- Forum-UserService/services/user_service.py
+++ Forum-UserService/services/user_service.py
@@ -9,7 +9,7 @@ from models.user import User
 from repos.user_repo import UserRepository
 from werkzeug.security import check_password_hash
 from sqlalchemy.exc import IntegrityError
-
+import requests
 
 
 
@@ -36,10 +36,20 @@ class UserService:
     def get_user_token_and_type(self, email: str, password: str):
         user = self.user_repository.get_user_by_email(email)
         if user and password:
-            token: str = jwt.encode(
-                {'user_id': user.userId, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60)},
-                config.JWT_SECURITY_KEY, "HS256")
-            return token, user.type
+            # token: str = jwt.encode(
+            #     {'user_id': user.userId, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60)},
+            #     config.JWT_SECURITY_KEY, "HS256")
+            # return token, user.type
+            auth_service_url = "http://127.0.0.1:5000/authentication/register"
+            payload = {
+                'user_id': user.userId
+            }
+            response = requests.post(auth_service_url, json=payload)
+            if response.status_code == 200:
+                data = response.json()
+                token = data.get('token')
+                user_status = data.get('user_status')
+                return token, user_status
 
         raise NotFoundException('User not found')
 
