800b88d6886fd2cb0daefd1d1a5d4f2f08181a45
jennz0 <anthonyznf@163.com>
2024-02-28 20:37:19 -0600

    post-reply aop decorator restored

--- Post-Reply-Service/aop/decorators.py
+++ Post-Reply-Service/aop/decorators.py
@@ -4,12 +4,8 @@ from repos.post_repo import PostRepository
 import jwt
 from flask import request
 
-<<<<<<< Updated upstream
 from .exceptions import AuthorizationError, InvalidTokenError
 import config
-=======
-from aop.exceptions import UnauthorizedException
->>>>>>> Stashed changes
 
 def token_required(f):
     @wraps(f)
@@ -17,7 +13,6 @@ def token_required(f):
         token = request.headers.get('Authorization')
         if not token:
             raise AuthorizationError("Authorization token is missing")
-<<<<<<< Updated upstream
 
         token_parts = token.split()
         if len(token_parts) == 2:    # "Bearer <token>" from Postman
@@ -25,15 +20,6 @@ def token_required(f):
         elif len(token_parts) == 1:
             token = token_parts[0]
 
-=======
-
-        token_parts = token.split()
-        if len(token_parts) == 2:    # "Bearer <token>" from Postman
-            token = token_parts[1]
-        elif len(token_parts) == 1:
-            token = token_parts[0]
-
->>>>>>> Stashed changes
         try:
             payload = jwt.decode(token, config.JWT_SECURITY_KEY, algorithms=['HS256'])
             # Add user_id and user_status to kwargs for easy access in route functions
@@ -48,30 +34,4 @@ def token_required(f):
 
         return f(*args, **kwargs)
 
-<<<<<<< Updated upstream
     return decorated_function
-=======
-    return decorated_function
-#
-# def token_required(f):
-#     @wraps(f)
-#     def decorator(self, *args, **kwargs):
-#         token = None
-#         if 'Authorization' in request.headers:
-#             token = request.headers['Authorization']
-#
-#         if not token:
-#             raise UnauthorizedException('Token is missing')
-#
-#         try:
-#             data = jwt.decode(token[7:], config.JWT_SECURITY_KEY, algorithms=["HS256"])
-#             current_user = UserRepository().get_user_by_Id(data['user_id'])
-#         except:
-#             raise UnauthorizedException('Token is invalid')
-#
-#         print(current_user)
-#
-#         return f(self, current_user, *args, **kwargs)
-#
-#     return decorator
->>>>>>> Stashed changes
