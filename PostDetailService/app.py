a627b9f9196612fe728cc3066a9638f25fc675f0
Jack <jackkong@bu.edu>
2024-03-02 16:23:37 -0500

    Add get all users api in user service for user management page purpose, set up the UserManagementPage and AdminHomePage for react, still need to implement the buttons

--- PostDetailService/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from routes.postDetail_blueprint import postDetail_blueprint
-
-app = Flask(__name__)
-app.register_blueprint(postDetail_blueprint)
-
-if __name__ == '__main__':
-    app.run()
--- PostHistoryService/routes/post_history_blueprint.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-from controllers.post_history_controller import PostHistoryController
-
-post_history_blueprint = Blueprint('post_history', __name__, url_prefix='/post_history')
-
-post_history_blueprint.add_url_rule('/', view_func=PostHistoryController.as_view('post_history'), methods=['GET'])
