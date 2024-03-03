5f61586c89c5641d36d85b9e9a6a26f51221651a
ruijayh <ruijiehuang98@gmail.com>
2024-03-02 18:29:38 -0800

    Implemented login page and register page.

---
+++ frontend-app/src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import LoginForm from './LoginForm';
+
+function LoginPage() {
+  const handleLogin = (token) => {
+    localStorage.setItem('token', token);
+    console.log('User logged in successfully');
+    window.location.href = '/';
+  };
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <LoginForm onLogin={handleLogin} />
+    </div>
+  );
+}
+
+export default LoginPage;
