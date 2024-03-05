973e9348a7ca47fe7a01a65a447203cf7266b465
Jack <jackkong@bu.edu>
2024-03-04 19:41:42 -0500

    turn user management react to redux form, fix some minor issue in Admin Home Page

--- frontend-app/src/store/store.jsx
+++ frontend-app/src/store/store.jsx
@@ -2,9 +2,12 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { thunk } from 'redux-thunk';
 import adminHomeReducer from './reducers/AdminHomeReducer';
+import userManagementReducer from './reducers/UserManagementReducer';
+
 
 const rootReducer = combineReducers({
-  adminHome: adminHomeReducer
+  adminHome: adminHomeReducer,
+  userManage: userManagementReducer
 });
 
 const store = createStore(
