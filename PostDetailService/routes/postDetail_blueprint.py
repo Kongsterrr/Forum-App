98462dfc6468eb1857eaf580b0d5a3c75b9893f8
ruijayh <ruijiehuang98@gmail.com>
2024-03-07 18:19:32 -0800

    Added get top posts in composite service.

--- PostDetailService/routes/postDetail_blueprint.py
+++ PostDetailService/routes/postDetail_blueprint.py
@@ -7,3 +7,4 @@ postDetail_blueprint = Blueprint('post-details', __name__, url_prefix='/post-det
 postDetail_blueprint.add_url_rule('/<int:post_id>', view_func=PostDetailView.as_view('post_details'), methods=['GET'])
 postDetail_blueprint.add_url_rule('/', view_func=UserHomeView.as_view('user_home'), methods=['GET'])
 postDetail_blueprint.add_url_rule('/admin', view_func=AdminHomeView.as_view('admin_home'), methods=['GET'])
+postDetail_blueprint.add_url_rule('/top', view_func=TopPostView.as_view('top_post'), methods=['GET'])
