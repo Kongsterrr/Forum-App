7c8bedf2e02431f0d9603136d9eff96b57c8aa0a
ruijayh <ruijiehuang98@gmail.com>
2024-03-07 22:27:25 -0800

    Modified email verification user status.

--- PostHistoryService/services/post_history_service.py
+++ PostHistoryService/services/post_history_service.py
@@ -11,6 +11,24 @@ class PostHistoryService:
         self.post_reply_url = 'http://127.0.0.1:5000/post_and_reply'
         self.user_service_url = 'http://127.0.0.1:5000/users'
 
+    # def get_post_history(self, user_id):
+    #     history_response = requests.get(self.history_url + '/' + str(user_id))
+    #     histories = history_response.json()["histories"]
+
+    #     post_history = []
+    #     for history in histories:
+    #         post_response = requests.get(self.post_reply_url + '/' + str(history["postId"]))
+    #         post = post_response.json()
+
+    #         # userId = post["userId"]
+    #         # user_response = requests.get(self.user_service_url + '/' + str(userId))
+    #         # user = user_response.json()
+    #         # post["firstName"] = user["firstName"]
+    #         # post["lastName"] = user["lastName"]
+    #         post["dateViewed"] = history["viewDate"]
+    #         post_history.append(post)
+    #     post_history.sort(key=lambda x: x["dateViewed"])
+    #     return post_history
     def get_post_history(self, user_id):
         history_response = requests.get(self.history_url + '/' + str(user_id))
         histories = history_response.json()["histories"]
@@ -19,6 +37,9 @@ class PostHistoryService:
         for history in histories:
             post_response = requests.get(self.post_reply_url + '/' + str(history["postId"]))
             post = post_response.json()
+            if "postId" not in post:
+                continue
+            print(post)
 
             userId = post["userId"]
             user_response = requests.get(self.user_service_url + '/' + str(userId))
